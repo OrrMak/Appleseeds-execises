231866d11c32ee7f85831bde6a6914e68a57bd0e
OrrMak <makonnenrocks@gmail.com>
2024-09-13 17:01:18 +0300

    m

--- WeekendExeJS.js
+++ WeekendExeJS.js
@@ -11,3 +11,21 @@ function yesOrNo(bool){
      return console.log(bool);
     }
 }
+
+//Ex2.1
+const exmArray= [19, 5, 42, 2, 77];
+// console.log(Math.min(...exmArray));
+function sumOfLowest(inputArray){
+    const copyInputArray=inputArray; // making a copy of the input array cause it's going to be spliced.
+    const min=Math.min(...copyInputArray); //finding the valueof min 1 
+    console.log(`${min}`);  //check
+    const minIndex=copyInputArray.indexOf(min); // finding the index of min1
+    console.log(`${minIndex}`); //check
+    copyInputArray.splice(minIndex,1);// deleting the min value
+    const min2=Math.min(...copyInputArray); // finding min2
+    console.log(`${min2}`); //check
+    return console.log(`${min+min2}`);
+}
+sumOfLowest(exmArray);
+
+//Ex2.2 - One and Zero - Binary
