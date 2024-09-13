002ad98b802484032595a12acabcf312bfb415a8
OrrMak <makonnenrocks@gmail.com>
2024-09-13 22:42:06 +0300

    m

--- WeekendExeJS.js
+++ WeekendExeJS.js
@@ -34,8 +34,6 @@ sumOfLowest(exmArray);
 // console.log(integerValue); 
 
 //Ex2.3 - Find the Next Perfect Square
-// let num=7;
-// console.log(typeof(num));
 
 function perfectSquare(num){
      // Check if num is not an integer
@@ -61,3 +59,32 @@ function perfectSquare(num){
     
 }
 perfectSquare(100);
+
+// Ex2.4 - Unique
+
+const findUniq= [ 1, 1, 1, 2, 1, 1 ];
+function uniqe(arr){
+    const min=Math.min(...arr);
+    const max=Math.max(...arr);
+    const sorted=arr.sort((a,b)=>a-b);
+    if(arr[0]===min && arr[1]===min){
+        return console.log(`the uniqe num is ${max}`)
+    }
+    else{
+        return console.log(`the uniqe num is ${min}`)
+    }
+}
+uniqe(findUniq)
+
+//Ex2.5 - Summation
+function Summation(n) {
+    if(n<1 | Number.isInteger(Math.sqrt(n))===false){
+        console.log(`The number needs to be a positive integer greater than 0`)
+    }
+    //if the given number is 1?
+    const array=Array.from({ length: n }, (v, i) => i+1); // _  It's a convention rather than a feature of the language itself.
+    console.log(array);
+
+    return console.log(array.reduce((accumulator, currentValue) => accumulator + currentValue,0))
+}
+Summation(4);
